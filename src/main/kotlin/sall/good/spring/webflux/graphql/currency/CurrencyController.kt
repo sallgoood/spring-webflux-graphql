37f18c6a07981c1fa54e3154c1fc146e33ca4f01
SallGood <32182117+sallgoood@users.noreply.github.com>
2023-08-16 14:11:38 +0900

    use @SchemaMapping

--- src/main/kotlin/sall/good/spring/webflux/graphql/currency/CurrencyController.kt
+++ src/main/kotlin/sall/good/spring/webflux/graphql/currency/CurrencyController.kt
@@ -4,6 +4,7 @@ import com.generated.graphql.Currency
 import org.slf4j.LoggerFactory
 import org.springframework.graphql.data.method.annotation.ContextValue
 import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 import sall.good.spring.webflux.graphql.currency.rate.CurrencyRateService
 import java.util.Locale
@@ -24,14 +25,18 @@ class CurrencyController(
         if (getCurrencies.hasError()) throw IllegalStateException("getCurrencies error")
         val currencies = getCurrencies.data?.let {
             it.map { (code, name) ->
-                val rates = rateService.getCurrencyRate(code)
                 Currency.builder()
                     .withCode(code)
                     .withName(name)
-                    .withRates(rates.data)
                     .build()
             }
         } ?: emptyList()
         return currencies
+    }
+
+    @SchemaMapping(typeName = "Currency", field = "rates")
+    suspend fun currencyRates(currency: Currency): Map<String, Any?>? {
+        val rates = rateService.getCurrencyRate(currency.code)
+        return rates.data
     }
 }
