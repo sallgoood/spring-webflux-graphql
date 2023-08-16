e8d9965484b0c7ff1f4aae5603ea078907bff44b
SallGood <32182117+sallgoood@users.noreply.github.com>
2023-08-16 12:23:12 +0900

    configure graphiql

--- src/main/kotlin/sall/good/spring/webflux/graphql/SecurityConfig.kt
+++ src/main/kotlin/sall/good/spring/webflux/graphql/SecurityConfig.kt
@@ -1,16 +1,19 @@
 package sall.good.spring.webflux.graphql
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 
-
-@SpringBootApplication
-class SpringWebfluxGraphqlApplication
-
-fun main(args: Array<String>) {
-    runApplication<SpringWebfluxGraphqlApplication>(*args)
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeExchange {
+                it.anyExchange().permitAll()
+            }
+            .build()
+    }
 }
